31e1c4276db67ba4bb27775231e3093af627e1ba
designersalim <designer.bd.salim@gmail.com>
2024-08-29 23:17:46 +0600

    first commit

---
+++ java.js
@@ -0,0 +1,77 @@
+// numeric
+
+var number = 20;
+
+console.log(number)
+
+// string
+var name = "Shakhawat Hossain";
+var first = "Shakhawt";
+var second = "Salim";
+
+var fullName = first + second
+
+console.log(name)
+console.log(fullName)
+
+// boolean
+var IsYourHobby = false;
+var isItYours = true;
+
+// snake case
+var is_it_yours = "No";
+
+// camel Case
+
+var isItYours = "Yes";
+
+// Pascale Case
+
+var IsItYours = false;
+
+// Variable Can't be start with numbers
+// Variable should be key sensitive
+// Can't contain a space or hyphen
+
+// parseInt
+
+var rubel = "10.75";
+
+var salim = parseInt(rubel)
+
+console.log(salim)
+
+var sohel = "10.5";
+
+console.log(sohel)
+var sahadat = parseFloat(sohel)
+
+console.log(sahadat)
+console.log(sahadat.toFixed(3))
+
+// practice
+var kola = 20;
+var mango = 10;
+mango += 15;
+var orange = 30;
+var onion = 25;
+var jinga = 45;
+var result1= kola + mango;
+
+
+
+var potato = orange - mango + onion;
+var rice = mango + jinga + onion;
+
+console.log(result1)
+console.log(mango)
+console.log(potato)
+console.log(rice)
+
+
+console.log(40+"20")
+console.log("40"+150)
+
+console.log(40-"20")
+
+console.log("40"-15)
